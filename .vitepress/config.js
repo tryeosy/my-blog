06331522dc1d0b50c27f852deb08ec5094ae0a2d
tryeosy <3493652109@qq.com>
2026-07-17 18:13:08 +0800

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,15 +1,22 @@
-export default {
-  // 核心修复：通知 VitePress 你的网站运行在子目录 /my-blog/ 下
-  // 注意：如果你的仓库名不叫 my-blog，请把下面两处的 my-blog 换成你实际的仓库名称！
-  base: '/my-blog/', 
-  
-  title: "我的个人博客",
-  description: "基于 GitHub Issues + VitePress",
-  themeConfig: { 
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  // GitHub Pages 地址：
+  // https://tryeosy.github.io/my-blog/
+  base: '/my-blog/',
+
+  lang: 'zh-CN',
+  title: '我的个人博客',
+  description: '基于 GitHub Issues + VitePress',
+
+  themeConfig: {
     sidebar: false,
-    // 修复左上角标题的点击跳转
+
     nav: [
-      { text: '首页', link: '/' }
+      {
+        text: '首页',
+        link: '/'
+      }
     ]
   }
-}
+})
